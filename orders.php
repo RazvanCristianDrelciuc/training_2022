94e4f08373845e9197dc7ac05e0927133e8e0952
RazvanCristianDrelciuc <114404515+RazvanCristianDrelciuc@users.noreply.github.com>
2022-11-15 17:20:23 +0200

    {#9}{ordes.php}{https://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-drelciuc/tasks/9}

--- orders.php
+++ orders.php
@@ -2,6 +2,8 @@
 
 require_once 'common.php';
 
+isAdmin();
+
 $sql = 'SELECT * from orders';
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
@@ -11,25 +13,22 @@ $orders = $stmt->fetchAll();
 
 <?php require_once 'header.php' ?>
 
-<div class="container">
-    <?php foreach ($orders as $order): ?>
-        <table>
-            <thead>
-            <tr>
-                <div class="productdetail">
-                    <th><?= __('Username') ?>: <?php echo($order['user_name']); ?></th>
-                    <th><?= __('Details') ?>: <?php echo($order['details']); ?></th>
-                    <th><?= __('Order date') ?>: <?php echo($order['order_date']); ?></th>
-                </div>
-                <form action="orders.php" method="post">
-                    <input type="hidden" name="order_id" value="<?= $order['id'] ?>">
+    <div class="container">
+        <?php foreach ($orders as $order): ?>
+            <table>
+                <thead>
+                <tr>
+                    <div class="productdetail">
+                        <th><?= __('Username') ?>: <?php echo($order['user_name']); ?></th>
+                        <th><?= __('Details') ?>: <?php echo($order['details']); ?></th>
+                        <th><?= __('Order date') ?>: <?php echo($order['order_date']); ?></th>
+                    </div>
                     <a href="order.php?id=<?= $order['id'] ?>"><?= __('View order') ?></a>
-                </form>
-            </tr>
-            </thead>
-        </table>
-    <?php endforeach; ?>
-    <a href="index.php"><?= __('Go to index ') ?></a>
-</div>
+                </tr>
+                </thead>
+            </table>
+        <?php endforeach; ?>
+        <a href="index.php"><?= __('Go to index ') ?></a>
+    </div>
 
 <?php require_once 'footer.php'; ?>
