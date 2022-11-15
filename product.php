3839ae0423a8dccfb5e20a73907156eaf05d42da
RazvanCristianDrelciuc <114404515+RazvanCristianDrelciuc@users.noreply.github.com>
2022-11-15 17:19:36 +0200

    {#7}{product.php}{https://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-drelciuc/tasks/8}

--- product.php
+++ product.php
@@ -2,34 +2,46 @@
 
 require_once 'common.php';
 
+isAdmin();
+
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+    //$id = $_GET['id'];
     $sql = 'SELECT * FROM `products` WHERE id=?';
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([$id]);
+    $stmt->execute([$_GET['id']]);
     $product = $stmt->fetch();
 }
 
-$case = 0;
+$case = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['edit_product'])) {
         $id = $_GET['id'];
         $sql = 'UPDATE  products  SET title=?, description=?, price=?, product_image=? WHERE id=?';
-        $case = 1;
+        $image_file = $_FILES["product_image"];
+        move_uploaded_file(
+            $image_file["tmp_name"],
+            __DIR__ . "/images/" . $image_file['name']
+        );
+        $case = 'edit';
     } else if (isset($_POST['add_product'])) {
-        $sql = 'INSERT INTO `products` (title, description, price, product_image) VALUES( ?, ?, ?,?)';
-        $case = 2;
+        $image_file = $_FILES["product_image"];
+        move_uploaded_file(
+            $image_file["tmp_name"],
+            __DIR__ . "/images/" . $image_file['name']
+        );
+        $sql = 'INSERT INTO `products` (title, description, price, product_image) VALUES ( ?, ?, ?,?)';
+        $case = 'add';
     }
     $id = $_GET['id'];
     $stmt = $pdo->prepare($sql);
 }
 
 switch ($case) {
-    case 1:
+    case 'edit':
         $stmt->execute([$_POST['product_name'], $_POST['description'], $_POST['price'], $_POST['product_image'], $id]);
         header('Location: products.php');
         exit;
-    case 2:
+    case 'add':
         $stmt->execute([$_POST['product_name'], $_POST['description'], $_POST['price'], $_POST['product_image']]);
         header('Location: products.php');
         exit;
@@ -39,20 +51,20 @@ switch ($case) {
 
 <?php require_once 'header.php' ?>
 
-<div class="formular">
-    <form action="product.php<?= isset($_GET['id']) ? '?id=' . $_GET['id'] . '' : '' ?>" method="post">
-        <?= __('Nume Produs') ?>: <input type="text" name="product_name" value="<?= isset($_GET['id']) ? $product['title'] : "" ?>"><br>
+<div class="form">
+    <form action="product.php<?= isset($_GET['id']) ? '?id=' . $_GET['id'] . '' : '' ?>" method="post" >
+        <?= __('Nume Produs') ?>: <input type="text" name="product_name" value="<?= $product['title'] ?? "" ?>"><br>
         <br>
-        <?= __('Descriere Produs') ?>: <input type="text" name="description" value="<?= isset($_GET['id']) ? $product['description'] : "" ?>"><br>
+        <?= __('Descriere Produs') ?>: <input type="text" name="description" value="<?= $product['description'] ?? "" ?>"><br>
         <br>
-        <?= __('Pret') ?>: <input type="number" name="price" value="<?= isset($_GET['id']) ? $product['price'] : "" ?>"><br>
+        <?= __('Pret') ?>: <input type="number" name="price" value="<?= $product['price'] ?? "" ?>"><br>
         <br>
         <?= __('Image') ?>:
         <div class="prodimage">
-            <img src="images/<?= isset($_GET['id']) ? $product['product_image'] : "" ?>">
+            <img src="images/<?= $product['product_image'] ?? "" ?>">
         </div>
         <br>
-        <?= __('Image') ?>: <input type="file" name="product_image">
+        <?= __('Image') ?>:<input type="file" name="product_image" >
         <br><br>
             <input type="submit" name="edit_product" value="<?= __('Edit product') ?>">
         <br>
