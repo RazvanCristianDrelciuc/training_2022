203d2267005c2329771d42cdae43d28257b3d1cc
RazvanCristianDrelciuc <114404515+RazvanCristianDrelciuc@users.noreply.github.com>
2022-11-15 17:20:59 +0200

    {#10}{order.php}{https://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-drelciuc/tasks/10}

--- order.php
+++ order.php
@@ -2,16 +2,28 @@
 
 require_once 'common.php';
 
+isAdmin();
+
 $sql = 'SELECT * FROM `items` WHERE order_id=?';
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$_GET['id']]);
 $items = $stmt->fetchAll();
+
+$sql2 = 'SELECT * from orders WHERE id=?';
+$stmt = $pdo->prepare($sql2);
+$stmt->execute([$_GET['id']]);
+$order = $stmt->fetch();
 
 ?>
 
 <?php require_once 'header.php' ?>
 
 <div class="container">
+        <div class="productdetail">
+            <th><?= __('Username') ?>: <?php echo($order['user_name']); ?></th>
+            <th><?= __('Details') ?>: <?php echo($order['details']); ?></th>
+            <th><?= __('Order date') ?>: <?php echo($order['order_date']); ?></th>
+        </div>
     <?php foreach ($items as $item): ?>
         <table>
             <thead>
