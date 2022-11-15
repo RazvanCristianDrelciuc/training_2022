e6e8e86aeab855ee6f92dbadf452c41c996265d1
RazvanCristianDrelciuc <114404515+RazvanCristianDrelciuc@users.noreply.github.com>
2022-11-15 17:18:07 +0200

    {#6}{login.php}{https://pm.5psolutions.net/public/index.php?path_info=projects/training-razvan-drelciuc/tasks/6}

--- login.php
+++ login.php
@@ -2,32 +2,29 @@
 
 require_once 'common.php';
 
-$error = ['nameErr' => 'Name is required',
-    'passErr' => 'Password is requierd',
-    'accErr' => 'THis Account doesnt exist'];
-
-$succes = 0;
+$username = $password = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $succes = 0;
-    if (empty($_POST['user_name']) || empty($_POST['password'])) {
-        $succes = 1;
+    $error = [];
+    if (empty($_POST['user_name'])) {
+        $error['nameErr'] = 'Name is required';
     }
-    $user_name = testInput($_POST['user_name']);
+    if (empty($_POST['password'])) {
+        $error['passErr'] = 'Pass is required';
+    }
+
+    $username = testInput($_POST['user_name']);
     $password = testInput($_POST['password']);
 
-    if ($succes == 0 && isset($user_name) && isset($password)) {
-        $sql = 'SELECT * FROM users where user_name=? limit 1';
+    if (empty($error)) {
+        $sql = 'SELECT * FROM users where user_name=? LIMIT 1';
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([$user_name]);
-        $users = $stmt->fetchALL();
+        $stmt->execute([$username]);
+        $user = $stmt->fetch();
 
-        foreach ($users as $user) {
-            if ($user['password'] == $password) {
-                $_SESSION['admin'] = $user['admin'];
-                $_SESSION['user_id'] = $user['user_id'];
-                $_SESSION['logged_in'] = true;
-                header('Location: index.php');
-            }
+        if ($user['password'] === $password) {
+            $_SESSION['admin'] = $user['admin'];
+            $_SESSION['user_id'] = $user['user_id'];
+            header('Location: index.php');
         }
     }
 }
@@ -41,13 +38,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <h1><?= __('Log in') ?></h1>
         <p><?= __('Please fill in this form to log to an account.') ?></p>
         <hr>
-        *<?= __('Name') ?>: <input type="text" name="user_name" value=""><br>
-        <?php if (empty($_POST["user_name"])): ?>
+        *<?= __('Name') ?>: <input type="text" name="user_name" value="<?= $username ?>"><br>
+        <?php if (!empty($error['nameErr'])): ?>
             <span>*<?= __($error['nameErr']); ?></span>
         <?php endif; ?>
         <br>
-        *<?= __('Password') ?>: <input type="text" name="password" value=""><br>
-        <?php if (empty($_POST['password'])): ?>
+        *<?= __('Password') ?>: <input type="password" name="password" value="<?= $password ?>"><br>
+        <?php if (!empty($error['passErr'])): ?>
             <span>*<?= __($error['passErr']); ?></span>
         <?php endif; ?>
         <br>
